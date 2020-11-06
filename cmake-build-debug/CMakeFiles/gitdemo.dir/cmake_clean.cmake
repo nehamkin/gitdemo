933e2ae622a397eeb829fc7da4c8859d6eaaefa2
nehamkin <nehamkin@post.bgu.ac.il>
2020-11-06 13:02:23 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/gitdemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitdemo.dir/main.cpp.o"
+  "gitdemo"
+  "gitdemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gitdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
